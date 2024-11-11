be303afc7d9307047d18dee0ff8c96bf7fb8a421
Meghana629 <meghanakatkam01@gmail.com>
2024-11-11 13:21:55 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,16 @@
+let str = "Hello World!";
+console.log(str.replace("World!" ,"My Dear Friend.."))
+
+let a="Hungry";
+console.log(a.repeat(10))
+
+
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"));
+
+let message = "Hello, world!";
+console.log(message.startsWith("hi"))
+
+
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"))
